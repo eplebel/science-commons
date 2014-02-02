f854c152c6f1ec41e0ca722c89cb8f9eefd9a06d
Christian Battista <battista.christian@gmail.com>
2014-02-02 13:08:06 -0800

    parsing and formatting of the stats field

--- config.py
+++ config.py
@@ -3,7 +3,23 @@
 ITEMS = ['doi','title','author','affiliation','journal','editor','reviewers','abstract','keywords','stats','ev','replicability','data','materials','prereg','disclosure','comments']
 
 def dummy(x):
-	return x
+	if x == 'NA':
+		return ''
+	else:
+		return x
+
+def stats(kw):
+	output = ""
+	count = 1
+	for study in kw:
+		output += "Study %i: " % count
+		for k in study.keys():
+			output += "%s = %s, " % (k, study[k])
+		output = output.rstrip(', ')
+		output += "<br/>"
+		count += 1
+
+	return output
 
 
 def keywords(kw):
@@ -15,5 +31,5 @@ def keywords(kw):
 
 formatter = dict.fromkeys(ITEMS, dummy)
 formatter['keywords'] = keywords
-
+formatter['stats'] = stats
 
--- update.py
+++ update.py
@@ -4,6 +4,7 @@ Update the dang ol' database
 """
 
 from mongo_tools import ReadFile, Connect
+from collections import OrderedDict
 
 #get the data
 ReadFile("articles.csv", "sciencecommons", "articles", clear=True)
@@ -11,15 +12,29 @@ ReadFile("articles.csv", "sciencecommons", "articles", clear=True)
 #now format the data for later use
 db = Connect("sciencecommons", "articles")
 
+#break up the stuff between the |
 for row in db.table.find():
 	for k in row.keys():
 		#split up the stuff separated by a |
 		if type(row[k]) == unicode and row[k].count('|'):
 			row[k] = row[k].split('|')
 	
+		print k
+
 	db.table.save(row)
 		
-
-
+#format the stats pile
+for row in db.table.find({'stats' : {'$exists':True}}):
+	stats = []
+	for stat in row['stats']:
+		d = OrderedDict()
+		st = stat.split(',')
+		for s in st:
+			k = s.split('=')[0].strip()
+			v = s.split('=')[1].strip()
+			d[k] = v
+		stats.append(d)
+	row['stats'] = stats
+	db.table.save(row)
 
 
