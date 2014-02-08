0eba73cf0a3173ac2605d3d194943f5fd865aa2b
Christian Battista <battista.christian@gmail.com>
2014-02-08 10:31:07 -0800

    make some html pages to ensure the db contents are OK

---
+++ page_tester.py
@@ -0,0 +1,35 @@
+#page_tester.py
+import mongo_tools as mt
+from config import *
+from formatter import formatter
+from mako_defs import *
+
+db = mt.Connect("sciencecommons", "articles")
+
+dois = db.table.find().distinct('doi')
+
+for doi in dois:
+		row = db.table.find_one({'doi': doi})
+
+		#format stuff, check if data present
+		data = {}
+	
+		for item in ITEMS:
+			if row.has_key(item):
+				data[item] = formatter[item](row[item])
+			else:
+				data[item] = ''
+
+		for badge in BADGES:
+			if row.has_key(badge):
+				data[badge] = formatter[badge](row[badge], badge)
+
+		output = article_template.render(index_url = index_url, data=data)
+
+		doi = doi.replace("/", "_")
+
+		f = open("test_html/%s.html" % doi, "w")
+
+		f.write(output)
+
+		f.close()  
