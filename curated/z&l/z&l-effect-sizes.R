d64dfb3f722c8c8aa1e93f9e5ece3c03d8b68191
Etienne P. LeBel <etienne.lebel@gmail.com>
2016-03-08 00:31:15 -0500

    z&l updates

--- curated/z&l/z&l-effect-sizes.R
+++ curated/z&l/z&l-effect-sizes.R
@@ -1,4 +1,5 @@
 require(compute.es)
+#target effect is positively coded: unethical act causes greater desire for cleaning compared to ethical act/control
 
 #Zhong & Liljenquist (2006) Study 2
 fes(6.99,14,13)
@@ -10,17 +11,23 @@ tes(.03, 76,77 )
 tes(-.79, 74,74 )
 #Earp et al. (2014) Study 3
 tes(-1.83, 131, 131)
+#Siev (2012) Study 1
+tes(-.7338, 165,170) #average of 5 t-values for 5 different products
+#Siev (2012) Study 2
+tes(-.93, 60,48)
+
 #Zhong & Liljenquist (2006) Study 3
 chies(4.57, 32)
 #Gamez et al. (2011) Study 3
 chies(.99, 45)
 #Fayard et al. (2009) Study 1
 chies(.23, 210)
+
 #Zhong & Liljenquist (2006) Study 4
 chies(5.02, 45)
 #Gamez et al. (2011) Study 4
 chies(1.04, 28)
 #Fayard et al. (2009) Study 2
 fes(.01,58,57) #they used continuous measure of volunteer behavior (in mins) instead of dichotomous yes/no
-
-
+#Reuven et al. (2013)
+fes(110.41, 29,29) #holy crap, equivalent to r=.81????#@ perhaps a mis-reported F-statistic?
--- curated/z&l/z&l.R
+++ curated/z&l/z&l.R
@@ -3,9 +3,9 @@ library(plotrix) #for ablineclip function
 setwd(getwd())
 
 ### to save as png file
-#png(filename="forest_plot_Z&L.png", res=95, width=1200, height=800, type="cairo")
+#png(filename="z&l.png", res=95, width=1200, height=800, type="cairo")
 
-dat <- read.csv(file = "zhongetal-replications.csv", header = TRUE)
+dat <- read.csv(file = "z&l-replications.csv", header = TRUE)
 
 dat <- escalc(measure="COR", ri=dat$r, ni=dat$N, data=dat, slab=dat$study)
 dat
@@ -15,13 +15,13 @@ par(mar=c(4,4,1,2))
 
 ### set up forest plot (rows argument used to specify exactly in which rows outcomes will be plotted)
 forest(dat$yi, dat$vi, xlim=c(-2.5, 2), at=c(-.5, -.25, 0, .25, .5, .75, 1),
-       cex=1.25, ylim=c(-1, 20), rows=c(3:5,8:10,13:17), subset=order(dat$study.num,decreasing=TRUE),
+       cex=1.25, ylim=c(-1, 23), rows=c(20:14,11:9,6:3),
        ilab=dat$N, ilab.xpos=-.75
        ,xlab="Effect size (r) [95% CI]", psize=1, pch=dat$study.symbol)
 op <- par(cex=1.25, font=3)
-text(-2.5, 19, "Studies and Replications",    pos=4)
-text( 2, 19, "Effect sizes (r) [95% CI]",  pos=2)
-text(-.75, 19, c("N"))
+text(-2.5, 22, "Studies and Replications",    pos=4)
+text( 2, 22, "Effect sizes (r) [95% CI]",  pos=2)
+text(-.75, 22, c("N"))
 
 ### set par back to the original settings
 par(op)
@@ -43,13 +43,14 @@ res.rep3 <- rma(yi=dat$yi, vi=dat$vi, data=dat, measure="COR", subset=(dat$study
                 method="FE")
 
 ### add summary polygons for the three subgroups
-addpoly(res.rep1, row=12, cex=1.25, mlab="Meta-analytic estimate of Study 2 replications")
-addpoly(res.rep2, row= 7, cex=1.25, mlab="Meta-analytic estimate of Study 3 replications")
+addpoly(res.rep1, row=13, cex=1.25, mlab="Meta-analytic estimate of Study 2 replications")
+addpoly(res.rep2, row= 8, cex=1.25, mlab="Meta-analytic estimate of Study 3 replications")
 addpoly(res.rep3, row= 2, cex=1.25, mlab="Meta-analytic estimate of Study 4 replications")
 
-ablineclip(v=.25, y1=0,y2=18, col="blue", lty="dashed", lwd=.5) #Simonsohn's small telescope line
+ablineclip(v=.25, y1=-2,y2=21, col="skyblue1", lty="dashed", lwd=.1) #Simonsohn's small telescope line
 op <- par(cex=.75, font=1)
-text(.57, .75, "r_33% small telescope", col="blue")
+text(.57, .75, "r_33% small telescope", col="skyblue1")
 
 #dev.off()
+
 
