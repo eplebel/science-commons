1e4fc1016233a0b2108c2a5dad9307884a75c83d
Etienne P. LeBel <etienne.lebel@gmail.com>
2016-04-06 00:26:36 -0400

    ego depletion updates

---
+++ curated/ego-depletion/RRR-metaanalysisRTV_incl.R
@@ -0,0 +1,74 @@
+#AnalysisDir <- 'c:/meta'
+### Set Working Directory
+#setwd(AnalysisDir)
+
+#import the metafor meta-nalysis R library. You will need to install it first.
+require("metafor")
+
+# Import the data
+RTVdat<- read.csv("RRR-RTV_incl.csv")
+#RTVdat<- RTVdat[order(RTVdat$study),]
+### random-effects model meta-analysis 
+# modeled after http://www.metafor-project.org/doku.php/tips:assembling_data_smd
+effectSizesAll<- escalc(measure="SMD", #standardized mean difference
+                     m1i= Ego.Depletion.Mean, m2i= Control.Mean,
+                     sd1i=Ego.Depletion.Std.Dev, sd2i= Control.Std.Dev,
+                     n1i=Ego.Depletion.Sample.size, n2i=Control.Sample.size,
+                     data= RTVdat)
+#(RTVdat$Ego.Depletion.Mean - RTVdat$Control.Mean) / RTVdat$Ego.Depletion.Std.Dev
+
+write.csv(effectSizesAll, file="RRR4-effect-sizes.csv")
+
+res <- rma(data=effectSizesAll, yi,vi,  method="REML", slab=paste(Study.name))
+
+#res <- rma(measure="RD", method="REML")
+
+#For a standard random-effects model, we need to add random effects for the trials, which can be done with:
+#  rma.mv(yi, vi, random = ~ 1 | trial, data=dat)
+
+###############################################################
+### FOREST PLOT WITH META-ANALYSIS ACROSS REPLICATION STUDIES
+###############################################################
+
+### Create the forest plot. 
+### Rows specify the rows in which the subsets of studies appear.
+### addfit=false eliminates the meta-analytic result for now. We'll first not calculate that
+### so we can create a forest plot that includes all the studies, even the original, and
+### then we'll calculate the meta-analysis based just on the RRR studies.
+
+### decrease margins so the full space is used and set the font size for the forest plot
+par(mar=c(4,4,1,2))
+par(cex=.8, font=1)
+
+#Create a forest plot that includes the original study
+forest(res,
+       ilab=cbind(RTVdat$Ego.Depletion.Mean, RTVdat$Control.Mean),
+       addfit=FALSE, #Don't include polygon because that would include original
+       ilab.xpos=c(-1.5,-1.2), #ylim=c(-1, 47) 
+       #xlim=c(-10,7)
+       #xlim=c(-12,5)
+       #xlim=c(-12,8)
+#      xlim=c(-5,2.5)
+      xlim=c(-3.5,2.5), psize=1, 
+
+)
+par("usr") #reveals xlims? http://r.789695.n4.nabble.com/tweaking-forest-plot-metafor-package-td4636818.html
+
+### Create a subset of the data that excludes Sripada et al. and renumber it
+RRRdat <- subset(RTVdat,Study != 0)
+rownames(repdata) <- NULL
+
+# modeled after http://www.metafor-project.org/doku.php/tips:assembling_data_smd
+effectSizesRRR<- escalc(measure="SMD", #standardized mean difference
+                        m1i= Ego.Depletion.Mean, m2i= Control.Mean,
+                        sd1i=Ego.Depletion.Std.Dev, sd2i= Control.Std.Dev,
+                        n1i=Ego.Depletion.Sample.size, n2i=Control.Sample.size,
+                        data= RRRdat)
+### run a meta-analysis on just the RRR studies
+res <- rma(data=effectSizesRRR, yi,vi,  method="REML", slab=paste(Study.name))
+
+### add a polygon to the Forest plot showing the meta-analytic effect of the replication studies
+addpoly(res, atransf=FALSE, row=0, cex=1.0, mlab="Meta-analytic effect for replications only")
+
+
+abline(h=0.45)
--- curated/ego-depletion/ego-depletion.R
+++ curated/ego-depletion/ego-depletion.R
@@ -3,33 +3,53 @@ library(plotrix) #for ablineclip function
 library(compute.es)
 
 #effect size calculations
-#gaillot et al. (2007) study 7
-r.gaillotetal2007 <- fes(5.04,31,30)
-r.cesario.corker <- fes(.122,60,59)
-r.wang.dvorak <- fes(15.61, 31,30)
-r.lange.eggert <- fes(1.12, 35,35)
-r.schmeicheletal <- tes(3.87, 12, 12) #total # correct
-r.pondetal <- tes(1.97, 65, 63) #from g=.35 from Carter et al.'s (2015) meta-analysis Table 1
-r.schmeichel2007 <- mes(32.88, 34.68, 4.15, 5.5, 41, 38) #total target words (OSPAN only)
-r.healyetal <- mes(54.3, 69.8, 12.2, 11.4, 19,19) #% of target words
-
-r.carter.mccullough.sucralose <- mes(39.8, 40.4, 6.8, 5.2, 45, 47)
-r.carter.mccullough.sucralose$r
-r.carter.mccullough.unsweetened <- mes(41.54, 39.38, 6.6, 5.9, 26, 24)
-r.carter.mccullough.unsweetened$r
-#weighted average of sucralose and unsweetened control conditions
-r.carter.mccullough = (r.carter.mccullough.sucralose$r * r.carter.mccullough.sucralose$N.total + r.carter.mccullough.unsweetened$r * r.carter.mccullough.unsweetened$N.total)/ (r.carter.mccullough.sucralose$N.total + r.carter.mccullough.unsweetened$N.total)
-r.lurquinetal <- tes(-1.46, 100,100)
-r.muravenetal <- mes(563, 758, 240, 280, 17, 17)
-r.murtagh.todd <- mes(628.88, 633.68, 354.42, 302.82, 26,25)
-r.sripadaetal <- tes(2.38, 24,23)
-
+gaillotetal2007study7 <- fes(5.04,31,30)
+d.gaillotetal2007study7 = gaillotetal2007study7$d
+v.gaillotetal2007study7 = gaillotetal2007study7$var.d
+cesario.corker2010 <- fes(.122,60,59)
+d.cesario.corker2010 = cesario.corker2010$d
+v.cesario.corker2010 = cesario.corker2010$var.d
+wang.dvorak2010 <- fes(15.61, 31,30)
+d.wang.dvorak2010 = wang.dvorak2010$d
+v.wang.dvorak2010 = wang.dvorak2010$var.d
+lange.eggert2014study1 <- fes(1.12, 35,35)
+d.lange.eggert2014study1 = lange.eggert2014study1$d
+v.lange.eggert2014study1 = lange.eggert2014study1$var.d
+schmeicheletal2003study1 <- tes(3.87, 12, 12) #total # correct
+d.schmeicheletal2003study1 = schmeicheletal2003study1$d
+v.schmeicheletal2003study1 = schmeicheletal2003study1$var.d
+d.pondetal2011study3 = .35 #from g=.35 from Carter et al.'s (2015) meta-analysis Table 1
+v.pondetal2011study3 = .07 #from v=.07 from Carter et al.'s (2015) meta-analysis Table 1
+schmeichel2007study1 <- mes(32.88, 34.68, 4.15, 5.5, 41, 38) #total target words (OSPAN only)
+d.schmeichel2007study1 = schmeichel2007study1$d
+v.schmeichel2007study1 = schmeichel2007study1$var.d
+healyetal2011study1 <- mes(54.3, 69.8, 12.2, 11.4, 19,19) #% of target words
+d.healyetal2011study1 = healyetal2011study1$d
+v.healyetal2011study1 = healyetal2011study1$var.d
+carter.mccullough.sucralose <- mes(39.8, 40.4, 6.8, 5.2, 45, 47)
+carter.mccullough.unsweetened <- mes(41.54, 39.38, 6.6, 5.9, 26, 24)
+#weighted average for d and v of sucralose and unsweetened control conditions
+d.carter.mccullough2013 = (carter.mccullough.sucralose$d * carter.mccullough.sucralose$N.total + carter.mccullough.unsweetened$d * carter.mccullough.unsweetened$N.total)/ (carter.mccullough.sucralose$N.total + carter.mccullough.unsweetened$N.total)
+v.carter.mccullough2013 = (carter.mccullough.sucralose$var.d * carter.mccullough.sucralose$N.total + carter.mccullough.unsweetened$var.d * carter.mccullough.unsweetened$N.total)/ (carter.mccullough.sucralose$N.total + carter.mccullough.unsweetened$N.total)
+lurquinetal2016 <- tes(-1.46, 100,100)
+d.lurquinetal2016 = lurquinetal2016$d
+v.lurquinetal2016 = lurquinetal2016$var.d
+muravenetal1998study2 <- mes(563, 758, 240, 280, 17, 17)
+d.muravenetal1998study2 = muravenetal1998study2$d
+v.muravenetal1998study2 = muravenetal1998study2$var.d
+murtagh.todd2004study2 <- mes(628.88, 633.68, 354.42, 302.82, 26,25)
+d.murtagh.todd2004study2 = murtagh.todd2004study2$d
+v.murtagh.todd2004study2 = murtagh.todd2004study2$var.d
+sripadaetal2014 <- tes(2.38, 24,23)
+d.sripadaetal2014 = sripadaetal2014$d
+v.sripadaetal2014 = sripadaetal2014$var.d
+#RRR4 ego depletion: 24 replication effect sizes (& var) as calculated from their own RRR-metaanalysisRTV_incl.R file
 
 ### to save as png file
-#png(filename="b&s2012.png", res=95, width=1200, height=800, type="cairo")
+#png(filename="ego-depletion.png", res=95, width=1200, height=800, type="cairo")
 
-dat <- read.csv(file = "b&s2012-replications(sorted).csv", header = TRUE)
-dat <- escalc(measure="COR", ri=dat$r, ni=dat$N, data=dat, slab=dat$study)
+dat <- read.csv(file = "ego-depletion.csv", header = TRUE)
+#dat <- escalc(measure="SMD", yi=dat$d, vi=dat$var, data=dat, slab=dat$study)
 dat
 
 ### decrease margins so the full space is used
@@ -51,7 +71,9 @@ par(op)
 par(op, cex=1,font=3)
 
 ### fit random-effects model (use slab argument to define study labels)
-res <- rma(yi=dat$yi, vi=dat$vi, data=dat, measure="COR", subset=(dat$study.type=="replication"),
+res <- rma(yi=dat$d, vi=dat$var, data=dat, measure="SMD", slab=dat$study) #it works!
+forest(res)
+res <- rma(yi=dat$yi, vi=dat$vi, data=dat, measure="SMD", subset=(dat$study.type=="replication"),
            slab=dat$study)
 ### add summary estimate to the bottom
 addpoly(res, row=-.75, cex=1.25, mlab="Random-effects meta-analytic estimate of all replications")
